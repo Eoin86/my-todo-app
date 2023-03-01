6bce0160aa65f572d41cedd4c49b077e0b111490
emahon <emahon@druidsoftware.com>
2023-03-01 12:50:51 +0000

    Small changes to web interface

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout='wide')
 
 def add_todo():
     todo = st.session_state['new_todo'] + "\n"
@@ -12,7 +13,11 @@ def add_todo():
 
 st.title('My Todo App')
 st.subheader('This is my todo app')
-st.write('This app is to improve your productivity')
+st.write('This app is to improve your <b>productivity</b>.',
+         unsafe_allow_html=True)
+
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,7 +27,3 @@ for index, todo in enumerate(todos):
         del  st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="", placeholder="Add new todo...",
-              on_change=add_todo, key='new_todo')
-
-print('hello')
